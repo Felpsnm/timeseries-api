40a9f2de0d6361c47f00be04fdf7aa22bafd299d
root <root@CSCO-W-PF2TNT6X.cisco.com>
2024-05-20 18:24:04 -0300

    added SmartCampus API

--- controller/smartcampus_controller.go
+++ controller/smartcampus_controller.go
@@ -4,26 +4,12 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/OpenDataTelemetry/timeseries-api/database"
 	"github.com/gin-gonic/gin"
 )
 
-func GetSmartLights(c *gin.Context) {
-	intervalStr := c.Query("interval")
-	interval, err := strconv.Atoi(intervalStr)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid interval value"})
-		return
-	}
-
-	if interval > 400 {
-		c.JSON(400, gin.H{"error": "Interval must be less than 400"})
-		return
-	}
-
+func GetWaterTankLevel(c *gin.Context) {
 	var objs = []gin.H{}
 	influxDB, err := database.ConnectToDB()
 
@@ -35,8 +21,9 @@ func GetSmartLights(c *gin.Context) {
 
 	query := `
 		SELECT *
-		FROM "SmartLight"
-		WHERE "time" >= now() - interval '` + intervalStr + ` minutes'
+		FROM "WaterTankLavel"
+		WHERE
+		time >= now() - interval '1 hour'
 		ORDER BY time DESC;
 	`
 
@@ -50,20 +37,23 @@ func GetSmartLights(c *gin.Context) {
 		value := iterator.Value() // Value of the current row
 		obj := gin.H{
 			"fields": gin.H{
-				"data_counter_0d_0":            value["data_counter_0d_0"],
-				"data_counter_0d_1":            value["data_counter_0d_1"],
+				"data_boardVoltage":            value["data_boardVoltage"],
 				"fCnt":                         value["fCnt"],
 				"rxInfo_altitude_0":            value["rxInfo_altitude_0"],
+				"rxInfo_altitude_1":            value["rxInfo_altitude_1"],
 				"rxInfo_latitude_0":            value["rxInfo_latitude_0"],
-				"rxInfo_loRaSNR_0":             value["rxInfo_loRaSNR_0"],
+				"rxInfo_latitude_1":            value["rxInfo_latitude_1"],
 				"rxInfo_longitude_0":           value["rxInfo_longitude_0"],
+				"rxInfo_longitude_1":           value["rxInfo_longitude_1"],
+				"rxInfo_loRaSNR_0":             value["rxInfo_loRaSNR_0"],
+				"rxInfo_loRaSNR_1":             value["rxInfo_loRaSNR_1"],
 				"rxInfo_rssi_0":                value["rxInfo_rssi_0"],
+				"rxInfo_rssi_1":                value["rxInfo_rssi_1"],
 				"txInfo_dataRate_spreadFactor": value["txInfo_dataRate_spreadFactor"],
 				"txInfo_frequency":             value["txInfo_frequency"],
-				"data_humidity":                value["data_humidity"],
-				"data_temperature":             value["data_temperature"],
+				"data_distance":                value["data_distance"],
 			},
-			"name": "SmartLights",
+			"name": "WaterTankLavel",
 			"tags": gin.H{
 
 				"applicationID":              value["applicationID"],
@@ -86,22 +76,24 @@ func GetSmartLights(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, objs)
 }
 
-func GetSmartLightbyNodeName(c *gin.Context) {
-	nodename := c.Param("nodename") // Parameter to query
-
-	var objs = []gin.H{} // Slice to store the query response in a list
+func GetHydrometer(c *gin.Context) {
+	var objs = []gin.H{}
 	influxDB, err := database.ConnectToDB()
+
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	defer influxDB.Close() // Close the client connection after the function ends
+	defer influxDB.Close()
+
 	query := `
 		SELECT *
-		FROM "SmartLights"
-		WHERE "nodeName" = '` + nodename + `'
+		FROM "Hidrometer"
+		WHERE
+		time >= now() - interval '1 hour'
 		ORDER BY time DESC;
 	`
+
 	iterator, err := influxDB.Query(context.Background(), query) // Create iterator from query response
 
 	if err != nil {
@@ -112,87 +104,25 @@ func GetSmartLightbyNodeName(c *gin.Context) {
 		value := iterator.Value() // Value of the current row
 		obj := gin.H{
 			"fields": gin.H{
-				"data_counter_0d_0":            value["data_counter_0d_0"],
-				"data_counter_0d_1":            value["data_counter_0d_1"],
+				"data_boardVoltage":            value["data_boardVoltage"],
 				"fCnt":                         value["fCnt"],
 				"rxInfo_altitude_0":            value["rxInfo_altitude_0"],
+				"rxInfo_altitude_1":            value["rxInfo_altitude_1"],
 				"rxInfo_latitude_0":            value["rxInfo_latitude_0"],
-				"rxInfo_loRaSNR_0":             value["rxInfo_loRaSNR_0"],
+				"rxInfo_latitude_1":            value["rxInfo_latitude_1"],
 				"rxInfo_longitude_0":           value["rxInfo_longitude_0"],
+				"rxInfo_longitude_1":           value["rxInfo_longitude_1"],
+				"rxInfo_loRaSNR_0":             value["rxInfo_loRaSNR_0"],
+				"rxInfo_loRaSNR_1":             value["rxInfo_loRaSNR_1"],
 				"rxInfo_rssi_0":                value["rxInfo_rssi_0"],
+				"rxInfo_rssi_1":                value["rxInfo_rssi_1"],
 				"txInfo_dataRate_spreadFactor": value["txInfo_dataRate_spreadFactor"],
 				"txInfo_frequency":             value["txInfo_frequency"],
-				"data_humidity":                value["data_humidity"],
-				"data_temperature":             value["data_temperature"],
+				"data_counter":                 value["data_counter"],
 			},
-			"name": "SmartLights",
+			"name": "Hydrometer",
 			"tags": gin.H{
 
-				"applicationID":              value["applicationID"],
-				"devEUI":                     value["devEUI"],
-				"fPort":                      value["fPort"],
-				"nodeName":                   value["nodeName"],
-				"rxInfo_mac_0":               value["rxInfo_mac_0"],
-				"rxInfo_name_0":              value["rxInfo_name_0"],
-				"txInfo_adr":                 "true",
-				"txInfo_codeRate":            "4/5",
-				"txInfo_dataRate_bandwidth":  "125",
-				"txInfo_dataRate_modulation": "LORA",
-			},
-			"timestamp": value["time"],
-		} // Convert the row to a gin.H map (JSON)
-		objs = append(objs, obj) // Append the row to the objs slice
-	}
-
-	if len(objs) == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "deviceName not found!"})
-		return
-	}
-
-	c.IndentedJSON(http.StatusOK, objs) // Return the objs slice as a JSON response
-}
-
-func GetSmartLightbyDevEUI(c *gin.Context) {
-	devEUI := c.Param("devEUI") // Parameter to query
-	var objs = []gin.H{}        // Slice to store the query response in a list
-	influxDB, err := database.ConnectToDB()
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	defer influxDB.Close() // Close the client connection after the function ends
-	query := `
-		SELECT *
-		FROM "SmartLights"
-		WHERE "devEUI" = '` + devEUI + `'
-		ORDER BY time DESC;
-	`
-	iterator, err := influxDB.Query(context.Background(), query) // Create iterator from query response
-
-	if err != nil {
-		panic(err)
-	}
-
-	for iterator.Next() {
-		value := iterator.Value()
-		obj := gin.H{
-			"fields": gin.H{
-				"data_counter_0d_0":            value["data_counter_0d_0"],
-				"data_counter_0d_1":            value["data_counter_0d_1"],
-				"fCnt":                         value["fCnt"],
-				"rxInfo_altitude_0":            value["rxInfo_altitude_0"],
-				"rxInfo_latitude_0":            value["rxInfo_latitude_0"],
-				"rxInfo_loRaSNR_0":             value["rxInfo_loRaSNR_0"],
-				"rxInfo_longitude_0":           value["rxInfo_longitude_0"],
-				"rxInfo_rssi_0":                value["rxInfo_rssi_0"],
-				"txInfo_dataRate_spreadFactor": value["txInfo_dataRate_spreadFactor"],
-				"txInfo_frequency":             value["txInfo_frequency"],
-				"data_humidity":                value["data_humidity"],
-				"data_temperature":             value["data_temperature"],
-			},
-			"name": "SmartLights",
-			"tags": gin.H{
 				"applicationID":              value["applicationID"],
 				"devEUI":                     value["devEUI"],
 				"fPort":                      value["fPort"],
@@ -206,11 +136,77 @@ func GetSmartLightbyDevEUI(c *gin.Context) {
 			},
 			"timestamp": value["time"],
 		}
-		objs = append(objs, obj)
+		// Convert the row to a gin.H map (JSON)
+		objs = append(objs, obj) // Append the row to the objs slice
 	}
-	if len(objs) == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "deviceId not found!"})
+	fmt.Println(len(objs))
+	c.IndentedJSON(http.StatusOK, objs)
+}
+
+func GetArtesianWell(c *gin.Context) {
+	var objs = []gin.H{}
+	influxDB, err := database.ConnectToDB()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	defer influxDB.Close()
+
+	query := `
+		SELECT *
+		FROM "ArtesianWell"
+		WHERE
+		time >= now() - interval '1 hour'
+		ORDER BY time DESC;
+	`
+
+	iterator, err := influxDB.Query(context.Background(), query) // Create iterator from query response
+
+	if err != nil {
+		panic(err)
+	}
+
+	for iterator.Next() { // Iterate over query response
+		value := iterator.Value() // Value of the current row
+		obj := gin.H{
+			"fields": gin.H{
+				"data_boardVoltage":            value["data_boardVoltage"],
+				"fCnt":                         value["fCnt"],
+				"rxInfo_altitude_0":            value["rxInfo_altitude_0"],
+				"rxInfo_altitude_1":            value["rxInfo_altitude_1"],
+				"rxInfo_latitude_0":            value["rxInfo_latitude_0"],
+				"rxInfo_latitude_1":            value["rxInfo_latitude_1"],
+				"rxInfo_longitude_0":           value["rxInfo_longitude_0"],
+				"rxInfo_longitude_1":           value["rxInfo_longitude_1"],
+				"rxInfo_loRaSNR_0":             value["rxInfo_loRaSNR_0"],
+				"rxInfo_loRaSNR_1":             value["rxInfo_loRaSNR_1"],
+				"rxInfo_rssi_0":                value["rxInfo_rssi_0"],
+				"rxInfo_rssi_1":                value["rxInfo_rssi_1"],
+				"txInfo_dataRate_spreadFactor": value["txInfo_dataRate_spreadFactor"],
+				"txInfo_frequency":             value["txInfo_frequency"],
+				"data_pressure_0":              value["data_pressure_0"],
+				"data_pressure_1":              value["data_pressure_1"],
+			},
+			"name": "ArtesianWell",
+			"tags": gin.H{
+
+				"applicationID":              value["applicationID"],
+				"devEUI":                     value["devEUI"],
+				"fPort":                      value["fPort"],
+				"nodeName":                   value["nodeName"],
+				"rxInfo_mac_0":               value["rxInfo_mac_0"],
+				"rxInfo_name_0":              value["rxInfo_name_0"],
+				"txInfo_adr":                 "true",
+				"txInfo_codeRate":            "4/5",
+				"txInfo_dataRate_bandwidth":  "125",
+				"txInfo_dataRate_modulation": "LORA",
+			},
+			"timestamp": value["time"],
+		}
+		// Convert the row to a gin.H map (JSON)
+		objs = append(objs, obj) // Append the row to the objs slice
+	}
+	fmt.Println(len(objs))
 	c.IndentedJSON(http.StatusOK, objs)
 }
--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/OpenDataTelemetry/timeseries-api/controller"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	r := gin.Default() // Create a new gin router instance
-	api := r.Group("/api/timeseries/v0.1/smartcampusmaua/SmartLights")
+	api_sl := r.Group("/api/timeseries/v0.1/smartcampusmaua/SmartLights")
 	{
-		api.GET("", controller.GetSmartLights)
-		api.GET("deviceName/:nodename", controller.GetSmartLightbyNodeName)
-		api.GET("deviceId/:devEUI", controller.GetSmartLightbyDevEUI)
+		api_sl.GET("", controller.GetSmartLights)
+		api_sl.GET("deviceName/:nodename", controller.GetSmartLightbyNodeName)
+		api_sl.GET("deviceId/:devEUI", controller.GetSmartLightbyDevEUI)
+	}
+
+	api_sc_wtl := r.Group("/api/timeseries/v0.1/smartcampusmaua/GetWaterTankLevel")
+	{
+		api_sc_wtl.GET("", controller.GetWaterTankLevel)
+	}
+
+	api_sc_h := r.Group("/api/timeseries/v0.1/smartcampusmaua/GetHydrometer")
+	{
+		api_sc_h.GET("", controller.GetHydrometer)
+	}
+
+	api_sc_aw := r.Group("/api/timeseries/v0.1/smartcampusmaua/GetArtesianWell")
+	{
+		api_sc_aw.GET("", controller.GetArtesianWell)
 	}
 
 	r.Run(":8888")
